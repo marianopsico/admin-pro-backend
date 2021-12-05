c0bd14dd49b694ecf9751cf34ae033d7adc6c870
System Administrator <root@MacBook-Pro-de-Mariano.local>
2021-12-05 13:00:47 -0300

    SPA Control

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors')
 const { dbConnection } = require('./database/config');
@@ -31,6 +31,12 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 app.use('/api/login', require('./routes/auth'));
+
+//! esto es por heroku sino no encuentra las rutas
+// lo último
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT );
